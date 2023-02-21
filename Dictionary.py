9637517dc61de4114ae0024b84fce2b3f4169f0b
Shavejansari <shavejmohd30@gmail.com>
2023-02-21 14:10:59 +0530

    first commit

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+#Dictionary is a collection of the key - value pairs.
+
+myDict = {
+    "Mohd" : "Is DevOps Engineer intern.",
+    "Harry" : "A coder",
+    "Marks" : [1, 2, 3, 5],
+    "anotherdict": {'Harry':'player'}
+}
+print(myDict['Marks'])
+print(myDict['Harry'])
+print(myDict['Mohd'])
+print(myDict['anotherdict']['Harry'])
---
+++ Dictionary_Methodes.py
@@ -0,0 +1,11 @@
+myDict = {
+    "mohd" : "Is DevOps Engineer intern.",
+    "harry" : "A coder",
+    "marks" : [1, 2, 3, 5],
+    "anotherdict": {'harry':'player'},
+    1: 2,
+    "devops": "Engineer"
+}
+print(list(myDict.keys())) #prints the keys of the dictionary
+print(list(myDict.values()))#prints the values of the dictionary
+print(type(myDict.keys()))#prints the keys of the dictionary
